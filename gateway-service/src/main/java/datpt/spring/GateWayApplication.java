506b03eb84fc0e66fa7bce31e6e8f3d8a2b5a5ce
datpt <datpham559@gmail.com>
2024-09-26 16:20:03 +0700

    unused zuul, use spring cloud gate way to switch proxy

--- gateway-service/src/main/java/datpt/spring/GateWayApplication.java
+++ gateway-service/src/main/java/datpt/spring/GateWayApplication.java
@@ -3,11 +3,10 @@ package datpt.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableZuulProxy
+//@EnableZuulProxy
 public class GateWayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GateWayApplication.class, args);
