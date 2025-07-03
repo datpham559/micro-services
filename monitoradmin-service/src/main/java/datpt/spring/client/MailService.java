8d86135379953afbb6aeddcc2bde4eb3fca0c03e
datpt <datpt@softdreams.vn>
2025-07-03 17:41:07 +0700

    add open feign call mail service from monitor service

--- monitoradmin-service/src/main/java/datpt/spring/client/MailService.java
+++ monitoradmin-service/src/main/java/datpt/spring/client/MailService.java
@@ -1,4 +1,14 @@
 package datpt.spring.client;
 
-public class MailService {
+import datpt.spring.service.GenerelMail;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "notificationService", fallback = MailService.class)
+public interface MailService {
+
+    @PostMapping(value = "/mail/send-general-mail")
+    void sendGeneralMail(@RequestBody GenerelMail generelMail);
+
 }
--- monitoradmin-service/src/main/java/datpt/spring/client/impl/MailServiceImpl.java
+++ monitoradmin-service/src/main/java/datpt/spring/client/impl/MailServiceImpl.java
@@ -1,4 +1,17 @@
 package datpt.spring.client.impl;
 
-public class MailService {
+import datpt.spring.client.MailService;
+import datpt.spring.service.GenerelMail;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailServiceImpl implements MailService {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public void sendGeneralMail(GenerelMail generelMail) {
+        logger.error("Something went wrong: {}", generelMail);
+    }
 }
